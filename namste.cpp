dc6ef6b0e780de84571cfe0fceb43e368f64ef7d
Piyush75812 <piyushkumar63072@gmail.com>
2025-02-08 11:57:36 +0530

    Add files via upload

---
+++ namste.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   /*int a=234;
+    cout<< a <<'\n';
+    char ch='abc';
+    cout<< ch << endl;
+    bool bl=true;
+    cout<<bl<<endl;
+    float f=1.2;
+    cout<<f<<endl;
+    double doub=1.23;
+    cout<<doub<<endl;
+    int size=sizeof(double);
+    cout<<"size of a is:"<<size<<endl; 
+   int a=8;
+    int b=9;
+    bool first= (a==b); 
+    cout<< first<<endl;
+    
+
+    bool second =(a<b);
+    cout<<second<<endl;
+
+    bool third =(a>b);
+    cout<<third<<endl;
+
+
+    bool fourth=(a<=b);
+    cout<<fourth<<endl;
+
+    bool fifth=(a>=b);
+    cout<<fifth<<endl;
+
+    bool sixth=(a!=b);
+    cout<<sixth<<endl;
+    */
+}
